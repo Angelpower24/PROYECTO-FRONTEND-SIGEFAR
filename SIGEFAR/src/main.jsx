f6acc80e3fa0ace3a12c1f565643c3ef718e019d
Angelpower24 <angel.floress@istpargentina.edu.pe>
2026-08-09 23:47:34 -0500

    Commit 4: Se empezó a trabajar "main.jsx" el archivo principal de entrada de React . Este importa StrictMode, createRoot, Bootstrap, App y BrowserRouter. Tambien renderiza la aplicación dentro del elemento root y permite utilizar el sistema de rutas.

--- SIGEFAR/src/main.jsx
+++ SIGEFAR/src/main.jsx
@@ -1,10 +1,22 @@
+// Importa el modo estricto de React y la función para renderizar la aplicación.
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+// Importa Bootstrap para estilos y funcionalidades.
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+// Importa el componente principal.
 import App from './App.jsx'
 
+// Permite manejar las rutas de la aplicación.
+import { BrowserRouter } from "react-router-dom";
+
+// Renderiza la aplicación en el elemento root.
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
